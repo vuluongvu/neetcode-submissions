7227eb0070cbeb1e02fd45ce850d9b546e1f3ab4
vuluongvu <luongvanvua2@gmail.com>
2026-06-18 21:42:05 +0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1;
+        int res = 0;
+        while (r < prices.size()){
+            int buy = prices[l];
+            int sell = prices[r];
+
+            if (buy < sell) {
+                int profit = sell - buy;
+                res = max(profit, res);
+            }
+            else {
+                l = r;
+            }
+            r++;
+        }
+
+        return res; 
+    }
+};
