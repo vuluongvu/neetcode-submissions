946e6187a877bfb486f912f37b5d4cd2ebd2d3fe
vuluongvu <luongvanvua2@gmail.com>
2026-08-27 09:55:37 +0700

    Add: buy-and-sell-crypto - submission-17

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-17.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxP = 0;
+        int minBuy = prices[0];
+        for (int &sell : prices){
+            maxP = max(sell - minBuy, maxP);
+            minBuy = min(minBuy, sell);
+        }
+        return maxP;
+    }
+};
