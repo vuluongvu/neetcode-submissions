a8e87d4f7f9bc083d03b06f2da526d574e96d79c
vuluongvu <luongvanvua2@gmail.com>
2026-09-03 14:35:05 +0700

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, int> countS;
+        unordered_map<char, int> countT;
+
+        for (int i = 0; i < s.size(); i++){
+            countT[t[i]]++;
+            countS[s[i]]++;
+        }
+
+        return countT == countS;
+    }
+};
