767b5573bad1cc67e3f2a138f70199555f372509
vuluongvu <luongvanvua2@gmail.com>
2026-09-23 00:23:22 +0700

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0; string res;
+        while(i < word1.size() && j < word2.size()){
+            res += word1[i++];
+            res += word2[j++];
+        }
+        res += word1.substr(i);
+        res += word2.substr(j);
+        return res;
+    }
+};
